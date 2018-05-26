2b635368ab7a4fa32609f09d0ce4f18eb2389511
AdrienHorgnies <aHorgnies@altissia.com>
2018-05-26 15:57:17 +0200

    test nonexistent file

--- src/test/java/LoadAndSeeDataFile/service/FileToTableParserTest.java
+++ src/test/java/LoadAndSeeDataFile/service/FileToTableParserTest.java
@@ -3,9 +3,11 @@ package LoadAndSeeDataFile.service;
 import LoadAndSeeDataFile.TestFileHelper;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+
 public class FileToTableParserTest {
 
-    private final TestFileHelper testFileHelper = new TestFileHelper("../../resources/filesToParseToTables");
+    private final TestFileHelper testFileHelper = new TestFileHelper("src/test/resources/files-to-parse");
 
     private static final String NONEXISTANT = "Wubbalubbadubdub.csv";
     private static final String EMPTY = "empty.csv";
@@ -13,9 +15,10 @@ public class FileToTableParserTest {
     private static final String NO_DATA = "no-data.csv";
     private static final String HAPPY_FLOW = "happy-flow.csv";
 
-    @Test
-    public void testParse_nonexistant() {
+    @Test(expected = FileNotFoundException.class)
+    public void testParse_nonexistant() throws FileNotFoundException {
         FileToTableParser parser = new FileToTableParser();
+        parser.parse(testFileHelper.getfile(NONEXISTANT));
     }
 
     @Test
